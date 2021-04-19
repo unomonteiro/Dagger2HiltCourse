e546b184cb9ef1d1e720c2fcf68388ca85369b06
Carlos <unomonteiro@gmail.com>
2021-04-19 23:08:18 +0100

    Lazy initialisation of global services

--- app/src/main/java/io/monteirodev/hiltcourse/common/composition/AppCompositionRoot.kt
+++ app/src/main/java/io/monteirodev/hiltcourse/common/composition/AppCompositionRoot.kt
@@ -1,5 +1,6 @@
 package io.monteirodev.hiltcourse.common.composition
 
+import androidx.annotation.UiThread
 import io.monteirodev.hiltcourse.Constants
 import io.monteirodev.hiltcourse.networking.StackoverflowApi
 import io.monteirodev.hiltcourse.questions.FetchQuestionDetailsUseCase
@@ -7,14 +8,19 @@ import io.monteirodev.hiltcourse.questions.FetchQuestionsUseCase
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
+@UiThread
 class AppCompositionRoot {
 
-    private val retrofit: Retrofit = Retrofit.Builder()
-            .baseUrl(Constants.BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
+    private val retrofit: Retrofit by lazy {
+        Retrofit.Builder()
+                .baseUrl(Constants.BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+    }
 
-    private val stackoverflowApi: StackoverflowApi = retrofit.create(StackoverflowApi::class.java)
+    private val stackoverflowApi: StackoverflowApi by lazy {
+        retrofit.create(StackoverflowApi::class.java)
+    }
 
     val fetchQuestionsUseCase get() = FetchQuestionsUseCase(stackoverflowApi)
 
